b268a964dca48a859358324e2b0f584fe35bbb15
pdeantihuman <pdeantihuman@gmail.com>
2018-05-24 01:14:39 +0800

    style: remove a lot of code

--- src/main.js
+++ src/main.js
@@ -3,17 +3,9 @@ import App from './App.vue'
 import router from './router/router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap'
-import $ from 'jquery';
-// window.jQuery = $;
-// window.$ = $;
-// global.jQuery = $;
-// const bootstrap = require('bootstrap');
 import iView from 'iview'
 Vue.use(iView)
-
-import axios from 'axios'
-import VueAxios from 'vue-axios'//实现axios全局引入
-Vue.use(VueAxios,axios)
+window.axios = require('axios')
 
 new Vue({
   el: '#app',
